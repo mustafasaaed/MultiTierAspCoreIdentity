af0dc37a49f94f16451c21204472e1c530f6adfa
Mustafa Saeed <mustafasaeed202019@gmail.com>
2019-03-20 21:45:02 +0200

    Adding Migration and Db Context

--- ECommerceTemplate.Data/AppDbContext.cs
+++ ECommerceTemplate.Data/AppDbContext.cs
@@ -9,16 +9,19 @@ namespace ECommerceTemplate.Data
     public class AppDbContext : DbContext
     {
 
-        public DbSet<ClaimBase> ClaimBases { get; set; }
-        public DbSet<Role> Roles { get; set; }
         public DbSet<RoleClaim> RoleClaims { get; set; }
-        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
         public DbSet<UserClaim> UserClaims { get; set; }
         public DbSet<UserLogin> UserLogins { get; set; }
-        public DbSet<UserLoginKey> UserLoginKeys { get; set; }
         public DbSet<UserRole> UserRoles { get; set; }
+        public DbSet<User> Users { get; set; }
         public DbSet<UserToken> UserTokens { get; set; }
-        public DbSet<UserTokenKey> UserTokenKeys { get; set; }
+
+
+
+        //public DbSet<ClaimBase> ClaimBases { get; set; }
+        //public DbSet<UserLoginKey> UserLoginKeys { get; set; }
+        //public DbSet<UserTokenKey> UserTokenKeys { get; set; }
 
 
         public AppDbContext(DbContextOptions<AppDbContext> options)
@@ -26,6 +29,25 @@ namespace ECommerceTemplate.Data
         {
 
         }        
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.Entity<UserLoginKey>()
+            //    .HasKey(ulk => new { ulk.LoginProvider, ulk.ProviderKey });
+
+            //modelBuilder.Entity<UserRole>()
+            //    .HasKey(ur => new { ur.RoleId, ur.UserId});
+
+            modelBuilder.Entity<UserLogin>()
+                .HasKey(ul => new { ul.UserId, ul.ProviderKey });
+
+            modelBuilder.Entity<UserRole>()
+                .HasKey(ur => new { ur.RoleId, ur.UserId });
+
+            modelBuilder.Entity<UserToken>()
+                .HasKey(ut => new { ut.UserId, ut.LoginProvider});
+
+        }
     }
 }
 
