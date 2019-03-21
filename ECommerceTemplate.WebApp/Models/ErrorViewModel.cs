8284ebc6490b3617860cf70d27d2bc6bd9d66645
Mustafa Saeed <mustafasaeed202019@gmail.com>
2019-03-21 02:23:41 +0200

    adding a new web app project

---
+++ ECommerceTemplate.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ECommerceTemplate.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
